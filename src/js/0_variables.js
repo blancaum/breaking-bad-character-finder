9d93ea2ed5e5a6fe4191521422351d7215911244
Blanca Ubis <ubis.blanca@gmail.com>
2022-11-12 17:25:26 +0100

    Add js files, variables and fetch

---
+++ src/js/0_variables.js
@@ -0,0 +1,23 @@
+'use strict';
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
+/***Elementos HTML***/
+
+//Search form
+const searchInput = document.querySelector('.js_search_input');
+const searchBtn = document.querySelector('.js_search_btn');
+
+//Lista de personajes
+const listCharactersElement = document.querySelector('.js_list_characters');
+//Lista de favoritos
+const listFavoritesElement = document.querySelector('.js_list_favorites');
+
+/***END Elementos HTML***/
+
+//Variables globales
+let allCharacters = [];
+let foundCharacters = [];
+let favCharacters = [];
+
+//Código que se ejecuta al cargar la página
---
+++ src/js/1_server.js
@@ -0,0 +1,13 @@
+'use strict';
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
+function fetchCharacters() {
+  fetch('https://breakingbadapi.com/api/characters')
+    .then((response) => response.json())
+    .then((data) => {
+      allCharacters = data;
+
+      //renderCharacters(allCharacters);
+    });
+}
