3edc3af300a25a24663b235c45290bc8be7c5925
Blanca Ubis <ubis.blanca@gmail.com>
2022-11-14 00:49:49 +0100

    Main responsive on-going

--- src/js/2_render.js
+++ src/js/2_render.js
@@ -11,16 +11,16 @@ function renderOneCharacter(character, classListItem) {
   if (characterInFavouritesIndex === -1) {
     html = `<li class="item">
   <article id="${character.char_id}" class="item__article ${classListItem}">
-    <img class="item__article__img" src="${character.img}" alt="${character.name}">
     <h3 class="item__article__title">${character.name}</h3>
+    <img class="item__article__img" src="${character.img}" alt="Photo of ${character.name}">
     <p class="item__article__text">${character.status}</p>
   </article>
 </li>`;
   } else {
     html = `<li class="item">
   <article id="${character.char_id}" class="item__article ${classListItem} favorite">
-    <img class="item__article__img" src="${character.img}" alt="${character.name}">
     <h3 class="item__article__title">${character.name}</h3>
+    <img class="item__article__img" src="${character.img}" alt="${character.name}">
     <p class="item__article__text">${character.status}</p>
   </article>
 </li>`;
